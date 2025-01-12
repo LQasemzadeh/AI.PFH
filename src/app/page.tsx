4e869a737959797c8d26a1a50134de07af8b7160
lqasemzadeh <lqasemzadeh@gmail.com>
2025-01-12 16:50:24 +0100

    Add Introduction component to the Home page
    Introduced a new `Introduction` component and integrated it into the Home page. The component currently contains a basic structure, ready for further development or content addition.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+"use client";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
+import Introduction from "@/app/Components/Introduction";
+
 
 export default function Home() {
-  return (
-      <div>
-          <Hero />
-          <About />
-      </div>
-  );
+    return (
+        <div>
+            <Hero />
+            <About />
+            <Introduction />
+        </div>
+    );
 }
